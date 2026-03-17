1ccd02724dbbcd488beb8e1c5bb29eba07ec6a99
basilpthomas <basilpthomas10@gmail.com>
2026-03-17 12:01:58 +0530

    dice commit

---
+++ lib/dice.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class Dice extends StatefulWidget {
+  const Dice({super.key});
+
+  @override
+  State<Dice> createState() => _DiceState();
+}
+
+class _DiceState extends State<Dice> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void callback() {
+    setState(() {
+      leftDiceNumber =  Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+    print('Left button got pressed');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Dice App"),
+          backgroundColor: Colors.blue,
+        ),
+        backgroundColor: Colors.blue,
+        body: Center(
+          child: Row(
+            children: [
+              Expanded(
+                child: TextButton(
+                    onPressed: () { callback();},
+                    child: Image.asset('assets/images/dice$leftDiceNumber.png')
+                )
+              ),
+              Expanded(
+                child: TextButton(
+                    onPressed: () {
+                      setState(() { callback(); });
+                      print('Right button got pressed');
+                    },
+                    child: Image.asset('assets/images/dice$rightDiceNumber.png')
+                )
+              ),
+            ],
+          ),
+        )
+      )
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'dice.dart';
 import 'miCard.dart';
 
 void main() {
   runApp(
-      MiCard()
+    // MiCard()
+    Dice()
   );
 }
 
