9faf2bc1a31b73a2aac3d8ef31abe2239ee475cd
basilpthomas <basilpthomas10@gmail.com>
2026-03-19 09:44:57 +0530

    destini commit

---
+++ lib/destini.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+import 'comp/storyBrain.dart';
+
+class Destini extends StatefulWidget {
+  const Destini({super.key});
+
+  @override
+  State<Destini> createState() => _DestiniState();
+}
+
+class _DestiniState extends State<Destini> {
+
+  StoryBrain storyBrain = StoryBrain();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.png'),
+            fit: BoxFit.cover,
+          ),),
+        padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                child: Center(
+                  child: Text(
+                    storyBrain.getStoryTitle(),
+                    style: TextStyle(
+                      fontSize: 25.0,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                color: Colors.red,
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      storyBrain.nextStory(1);
+                    });
+                  },
+                  child: Text(
+                    storyBrain.getChoice1(),
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0,),
+              Container(
+                color: Colors.blue,
+                child: Visibility(
+                  visible: storyBrain.buttonShouldBeVisible(),
+                  child: TextButton(
+                    onPressed: () {
+                      setState(() {
+                        storyBrain.nextStory(2);
+                      });
+                    },
+                    child: Text(
+                      storyBrain.getChoice2(),
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mi_card/quizzler.dart';
 import 'package:mi_card/xylophone.dart';
+import 'destini.dart';
 import 'dice.dart';
 import 'magic8Ball.dart';
 import 'miCard.dart';
@@ -8,12 +9,14 @@ import 'miCard.dart';
 void main() =>
   runApp(
     MaterialApp(
-      home: Quizzler()
+        theme: ThemeData.dark(),
+        home: Destini()
     )
     // MiCard()
     // XylophoneApp()
     // Dice()
     //   BallPage(),
+    //   Quizzler()
   );
 
 
